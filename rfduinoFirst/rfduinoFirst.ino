7a06287032c3628606378a564e7927d21d6598a6
tannoyoshinobu <tannoyoshinobu@gmail.com>
2015-12-11 17:06:48 +0900

    RFduino add UUID

--- rfduinoFirst/rfduinoFirst.ino
+++ rfduinoFirst/rfduinoFirst.ino
@@ -10,6 +10,9 @@ void setup()
   Serial.begin(9600);
   RFduinoBLE.advertisementData = "myData"; // shouldnt be more than 10 characters long
   RFduinoBLE.deviceName = "myRFduino"; // name of your RFduino. Will appear when other BLE enabled devices search for it
+
+  //ref https://www.uuidgenerator.net/
+  RFduinoBLE.customUUID = "9a444680-9fd8-11e5-8994-feff819cdc9f"; 
   RFduinoBLE.begin(); // begin
 }
 
